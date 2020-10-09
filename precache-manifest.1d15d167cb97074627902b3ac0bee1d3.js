3d05b38c2d58177d074eb12d07fc47ef65b5badd
Scim <ryanedmondbeck@gmail.com>
2020-10-09 11:50:58 -0500

    Updates

---
+++ precache-manifest.1d15d167cb97074627902b3ac0bee1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b753ca65779dffe8480fe953da9df729",
+    "url": "/human-detection-app/index.html"
+  },
+  {
+    "revision": "2aefed9b26d2f8bdca1e",
+    "url": "/human-detection-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "753c874f52e7ce39b906",
+    "url": "/human-detection-app/static/js/2.53083674.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/human-detection-app/static/js/2.53083674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aefed9b26d2f8bdca1e",
+    "url": "/human-detection-app/static/js/main.befaf8d0.chunk.js"
+  },
+  {
+    "revision": "d4a55ade33388332b94b",
+    "url": "/human-detection-app/static/js/runtime-main.3c9f479b.js"
+  }
+]);
